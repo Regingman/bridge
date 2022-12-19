7906d01dc36b11ab858eae3ad69d3b90e7d3b158
Атай <51788708+Regingman@users.noreply.github.com>
2022-12-19 21:49:08 +0600

    Update VerifyCodeResponse.cs

--- MyDataCoinBridge/Models/VerifyCodeResponse.cs
+++ MyDataCoinBridge/Models/VerifyCodeResponse.cs
@@ -8,13 +8,15 @@
 
     public class VerifyCodeResponse
     {
-        public VerifyCodeResponse(Tokens tokens, int code, string errorMessage, string token, bool isVerified)
+        public VerifyCodeResponse(Tokens tokens, int code, string errorMessage, string token, bool isVerified, string role)
         {
             Tokens = tokens;
             Code = code;
             ErrorMessage = errorMessage;
             Token = token;
             IsVerified = isVerified;
+            Role = role;
+
         }
         public VerifyCodeResponse(Tokens tokens, int code, string errorMessage)
         {
@@ -28,7 +30,7 @@
         public int Code { get; set; }
 
         public string ErrorMessage { get; set; }
-
+        public string Role { get; set; }
         public string Email { get; set; }
         public string Token { get; set; }
         public bool IsVerified { get; set; }
